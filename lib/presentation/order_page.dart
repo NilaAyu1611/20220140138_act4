c41d738d52ff0abbf0f033887040d55525f6d1d3
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-23 21:07:31 +0700

    buat form jumlah pesan makanan

--- lib/presentation/order_page.dart
+++ lib/presentation/order_page.dart
@@ -53,7 +53,17 @@ class _OrderPageState extends State<OrderPage> {
                   return null;
                 },
               ),
-               
+               TextFormField(
+                controller: jumlahMakananController,
+                decoration: const InputDecoration(labelText: 'Food QTY Order'),
+                validator: (value) {
+                  if (value == null || value.isEmpty){
+                    return 'Please enter your qty of food order';
+                  }
+                  return null;
+                },
+              ),
+              
              
             ],
           ),
