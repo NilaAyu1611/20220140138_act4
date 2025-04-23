fdb8718a53764dd49f8b0fba1cbfb25d7731de30
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-23 21:41:45 +0700

    konfigurasi rute navigasi

--- lib/presentation/order_page.dart
+++ lib/presentation/order_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/detailorder_page.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
