32993e63aba80ef5bdcdb154f6acacbd74a0ddbd
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-23 21:54:14 +0700

    perbaikan typo

--- lib/presentation/detailorder_page.dart
+++ lib/presentation/detailorder_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:play_navigation/presentation/home_page.dart';
 
-class DetailOrderPage extends StatefulWidget {
+class DetailOrderPage extends StatelessWidget {
   final String makanan;
   final String minuman;
   final String jumlahMakanan;
