e3a9b382765d4a7dd13aee6ca0163d3edb262e4c
NilaAyu1611 <nilaayucw@gmail.com>
2025-04-23 21:29:23 +0700

    tampilan data pesanan di body

--- lib/presentation/detailorder_page.dart
+++ lib/presentation/detailorder_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
 
 class DetailOrderPage extends StatefulWidget {
   final String makanan;
@@ -13,15 +14,33 @@ class DetailOrderPage extends StatefulWidget {
     required this.jumlahMakanan,
     required this.jumlahMinuman,
     required this.totalHarga,
-    });
+  }); 
 
-  @override
-  State<DetailOrderPage> createState() => _DetailOrderPageState();
-}
-
-class _DetailOrderPageState extends State<DetailOrderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(          
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+
+            const SizedBox(height: 20),
+            
+          ],
+        ),
+      ),
+    );
   }
+
+
 }
+
+
+  
